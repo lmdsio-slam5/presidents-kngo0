c86d1ef30d57f7182535fc33532a35dd892a4e12
kngo0 <kevin.ngo707@hotmail.com>
2015-12-08 13:21:46 +0100

    Add OpenShift

--- app/config/prod.php
+++ app/config/prod.php
@@ -1,5 +1,12 @@
 <?php
 
+// MySQL config for OpenShift
+$dbHost = getenv('OPENSHIFT_MYSQL_DB_HOST');
+$dbPort = getenv('OPENSHIFT_MYSQL_DB_PORT');
+$dbName = getenv('OPENSHIFT_GEAR_NAME');
+$dbUser = getenv('OPENSHIFT_MYSQL_DB_USERNAME');
+$dbPassword = getenv('OPENSHIFT_MYSQL_DB_PASSWORD');
+
 // Doctrine (db)
 $app['db.options'] = array(
     'driver'   => 'pdo_mysql',
